7af03de79ede1af0613475a468d901659be92084
Eric Rodrigo de Freitas <eric.freitas@objective.com.br>
2021-12-03 18:13:15 -0300

    add generated project from spring initializr

---
+++ src/test/java/br/com/eric/herokuspringhelloworld/HerokuSpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eric.herokuspringhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuSpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
